f1b20fd7c85911695eab0937c42b5af4b16a0881
דור שפירא <Dor.Shapira@outlook.com>
2023-01-06 18:16:27 +0200

    added core_basic_window and core_basic_window_web examples

---
+++ examples/core/core_basic_window.py
@@ -0,0 +1,54 @@
+"""
+
+raylib [core] example - Basic Window
+
+"""
+
+from raypyc import *
+
+
+# ------------------------------------------------------------------------------------
+# Program main entry point
+# ------------------------------------------------------------------------------------
+def main():
+    # Initialization
+    # ------------------------------------------------------------------------------------
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 450
+
+    init_window(SCREEN_WIDTH, SCREEN_HEIGHT, b"raylib [core] example - basic window")
+
+    # TODO: Load resources / Initialize variables at this point
+
+    set_target_fps(60)  # Set our game to run at 60 frames-per-second
+    # ------------------------------------------------------------------------------------
+
+    # Main game loop
+    while not window_should_close():  # Detect window close button or ESC key
+        # Update
+        # ----------------------------------------------------------------------------------
+        # TODO: Update variables / Implement example logic at this point
+        # ----------------------------------------------------------------------------------
+
+        # Draw
+        # ----------------------------------------------------------------------------------
+        begin_drawing()
+
+        clear_background(RAYWHITE)
+        draw_text(b"Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY)
+
+        end_drawing()
+        # ----------------------------------------------------------------------------------
+
+    # De-Initialization
+    # ----------------------------------------------------------------------------------
+
+    # TODO: Unload all loaded resources at this point
+
+    close_window()  # Close window and OpenGL context
+    # ----------------------------------------------------------------------------------
+
+
+# Execute the main function
+if __name__ == '__main__':
+    main()
---
+++ examples/core/core_basic_window_web.py
@@ -0,0 +1,39 @@
+import asyncio
+from raypyc import *
+
+
+# ------------------------------------------------------------------------------------
+# Program main entry point
+# ------------------------------------------------------------------------------------
+async def main():
+    # Initialization
+    # ------------------------------------------------------------------------------------
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 450
+
+    init_window(SCREEN_WIDTH, SCREEN_HEIGHT, b"raylib [core] example - basic window")
+
+    # TODO: Load resources / Initialize variables at this point
+
+    set_target_fps(60)  # Set our game to run at 60 frames-per-second
+    # ------------------------------------------------------------------------------------
+
+    # Main game loop
+    while True:
+        # Update
+        # ----------------------------------------------------------------------------------
+        # TODO: Update variables / Implement example logic at this point
+        # ----------------------------------------------------------------------------------
+
+        # Draw
+        # ----------------------------------------------------------------------------------
+        begin_drawing()
+
+        clear_background(RAYWHITE)
+        draw_text(b"Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY)
+
+        end_drawing()
+        # ----------------------------------------------------------------------------------
+        await asyncio.sleep(0)
+
+asyncio.run(main())
