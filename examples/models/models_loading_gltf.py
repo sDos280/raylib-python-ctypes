12357f2b8f510ae47035cc7768f3c13a853621b5
דור שפירא <Dor.Shapira@outlook.com>
2023-01-28 00:27:31 +0200

    tried to add models_loading_gltf example and started working on headers modified

---
+++ examples/models/models_loading_gltf.py
@@ -0,0 +1,90 @@
+"""
+
+raylib [models] example - loading gltf with animations
+
+"""
+
+from raypyc import *
+from ctypes import *
+
+# Definitions
+# ------------------------------------------------------------------------------------
+MAX_BUILDINGS = 100
+
+
+# ------------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------------
+# Program main entry point
+# ------------------------------------------------------------------------------------
+def main():
+    # Initialization
+    # ------------------------------------------------------------------------------------
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 450
+
+    init_window(SCREEN_WIDTH, SCREEN_HEIGHT, b"raylib [models] example - loading gltf")
+
+    camera = Camera()
+    camera.position = Vector3(5.0, 5.0, 5.0)  # Camera position
+    camera.target = Vector3(0.0, 2.0, 0.0)  # Camera looking at point
+    camera.up = Vector3(0.0, 1.0, 0.0)  # Camera up vector (rotation towards target)
+    camera.fovy = 45.0  # Camera field-of-view Y
+    camera.projection = CameraProjection.CAMERA_PERSPECTIVE  # Camera mode type
+
+    # Load gltf model
+    model = load_model(b"resources/models/gltf/robot.glb")
+    animsCount = c_uint()
+    animIndex = int(0)
+    animCurrentFrame = int(0)
+    array = POINTER()
+    modelAnimations = load_model_animations(b"resources/models/gltf/robot.glb", array)
+    print(array)
+    position = Vector3(0.0, 0.0, 0.0)  # Set model position
+    set_camera_mode(camera, CameraMode.CAMERA_FREE)  # Set free camera mode
+
+    set_target_fps(60)  # Set our game to run at 60 frames-per-second
+    # ------------------------------------------------------------------------------------
+
+    # Main game loop
+    while not window_should_close():  # Detect window close button or ESC key
+        # Update
+        # ----------------------------------------------------------------------------------
+        # Select current animation
+        if is_key_pressed(KeyboardKey.KEY_UP): animIndex = (animIndex + 1) % animsCount
+        if is_key_pressed(KeyboardKey.KEY_DOWN): animIndex = (animIndex + animsCount - 1) % animsCount
+
+        # Update model animation
+        anim = modelAnimations[int(gg)]
+        animCurrentFrame = (animCurrentFrame + 1) % anim.frameCount
+        update_model_animation(model, anim, animCurrentFrame)
+
+        # Update camera
+        update_camera(pointer(camera))
+        # ----------------------------------------------------------------------------------
+
+        # Draw
+        # ----------------------------------------------------------------------------------
+        begin_drawing()
+
+        clear_background(RAYWHITE)
+
+        begin_mode_3d()
+
+        draw_model(model, position, 1.0, WHITE)  # Draw animated model
+        draw_grid(10, 1.0)
+
+        end_mode_3d()
+
+        end_drawing()
+        # ----------------------------------------------------------------------------------
+
+    # De-Initialization
+    # ----------------------------------------------------------------------------------
+    close_window()  # Close window and OpenGL context
+    # ----------------------------------------------------------------------------------
+
+
+# Execute the main function
+if __name__ == '__main__':
+    main()
