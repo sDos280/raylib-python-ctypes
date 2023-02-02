18374108694cb9039240d9e9cc74735731384974
דור שפירא <Dor.Shapira@outlook.com>
2023-02-02 13:38:56 +0200

    added manual build script for window!!
    thanks @stucotso

---
+++ build/build_all_windows.cmd
@@ -0,0 +1,42 @@
+:: clone raylibs repositories
+::git clone --depth 1 https://github.com/raysan5/raylib
+::git clone --depth 1 https://github.com/raysan5/raudio
+::git clone --depth 1 https://github.com/raysan5/raygui
+
+:: compile raudio
+gcc -c raylib/src/raudio.c -std=c99 -Iraudio/src
+ar rcs raudio.a raudio.o
+
+:: compile raypyc extra functions
+gcc -c raypyc_extra_functions.c -std=c99 -Iraudio/src -lraudio -DRAUDIO_STANDALONE
+gcc -shared -o raypyc_extra_functions.dll raypyc_extra_functions.o
+move raypyc_extra_functions.dll all
+rm *.a *.o
+
+:: compile raylib as dynamic library
+cd "./raylib/src"
+windres raylib.dll.rc -o raylib.dll.rc.data
+make RAYLIB_LIBTYPE=SHARED RAYLIB_MODULE_RAYGUI=TRUE GRAPHICS=GRAPHICS_API_OPENGL_43
+cd ../..
+cp raylib/src/raylib.dll all
+cd "./raylib/src"
+make clean
+cd ../..
+
+:: preprocess raylib headers
+python .\rlhpp.py raylib/src/raylib.h RLAPI
+python .\rlhpp.py raylib/src/raymath.h RMAPI
+python .\rlhpp.py raylib/src/rlgl.h RLAPI
+python .\rlhpp.py raygui/src/raygui.h RAYGUIAPI
+
+:: parse raylib headers
+cp "./raylib/src/config.h" "./tmp/config.h.preprocessed"
+cd raylib/parser
+make raylib_parser
+.\raylib_parser --input "../../tmp/raylib.h.preprocessed" --output "../../all/raylib_api.json" --define RLAPI --format JSON
+.\raylib_parser --input "../../tmp/raymath.h.preprocessed" --output "../../all/raymath_api.json" --define RMAPI --format JSON
+.\raylib_parser --input "../../tmp/rlgl.h.preprocessed" --output "../../all/rlgl_api.json" --define RLAPI --format JSON
+.\raylib_parser --input "../../tmp/raygui.h.preprocessed" --output "../../all/raygui_api.json" --define RAYGUIAPI --format JSON
+.\raylib_parser --input "../../tmp/config.h.preprocessed" --output "../../all/config_api.json" --format JSON
+cd ../..
+rm -rf tmp
---
+++ build/rlhpp.py
@@ -0,0 +1,126 @@
+# https://gist.github.com/stucotso/76f602a267be052ec19686c38a783f65
+# thanks https://gist.github.com/stucotso !!!
+
+import sys
+from pycparser import parse_file, c_ast
+
+import os
+
+
+class ast:
+    def __init__(self, children=[], params=[], line=0):
+        self.children = children
+        self.params = params
+        self.line = line
+
+    def expand(self, flags, nest):
+        return "".join([i.expand(flags, nest + 1) for i in self.children])
+
+    def dump(self, nest=0):
+        ret = ("\t" * nest) + self.__class__.__name__ + "\n"
+        for i in self.children:
+            ret += i.dump(nest + 1)
+        return ret
+
+
+class ifdef(ast):
+    def evaluate(self, flags):
+        func = self.params[0].strip()
+        negate = False
+        if func == "#ifndef": negate = True
+        key = self.params[1].strip()
+        test = key in flags.keys() and flags[key] != ""
+        if negate: return not test
+        return test
+
+    def expand(self, flags, nest):
+        result = ""
+
+        if self.evaluate(flags):  #
+            for i in self.children:
+                child = i.expand(flags, nest + 1)
+                if child.strip().split(" ")[0].strip() != "#endif":
+                    result += child
+        return result
+
+
+class text(ast):
+    def expand(self, flags, nest): return self.params[0]
+
+
+class Preprocessor():
+    def __init__(self, flags):
+        self.index = 0
+        self.flags = flags
+
+    def preprocessLine(self, lines, parent):
+
+        if self.index >= len(lines):
+            self.index = -1
+            return text(line=-1, params=[""])
+
+        line = lines[self.index]
+
+        directive = None
+        if line.strip().startswith("#"):
+            directive = line.strip().split(" ")
+            if directive[0].strip() in ["#ifdef", "#ifndef", "#if"]:
+                self.index += 1
+                newparent = ifdef(params=directive, line=self.index, children=[])
+                parent.children.append(newparent)
+                self.preprocessLines(lines, newparent)
+                return newparent
+
+        self.index += 1
+
+        result = text(line=self.index, params=[line])
+
+        parent.children.append(result)
+        return result
+
+    def preprocessLines(self, lines, parent):
+        while self.index >= 0:
+            item = self.preprocessLine(lines, parent)
+            if item.line == -1 or item.params[0].strip() == "#endif" or item.params[0].split(" ")[0] == "#endif":
+                return
+
+    def preprocessFile(self, f):
+        print("Preproccessing file:", f)
+        file = open(f, 'r')
+        lines = file.readlines()
+        self.index = 0
+        result = ast(children=[])
+        self.preprocessLines(lines, result)
+        if self.index < len(lines) and self.index >= 0: raise Exception("error parsing, failed at: " + str(self.index))
+        return result.expand(self.flags, 0)  # self.expand(result.children)
+
+
+def printUsage():
+    print("you need to use this script like that:")
+    print(f"{sys.argv[0][3:]} path_to_header flag_that_you_want_to_preserve")
+
+
+header = [
+    sys.argv[1]
+]
+
+params = [
+    f"-d {sys.argv[2]}"
+]
+
+
+def preprocess():
+    if not os.path.exists("./tmp"):
+        os.system("mkdir tmp")
+    for i in header:
+        fname = i.split("/")[-1]
+
+        output = Preprocessor({}).preprocessFile(i)
+        outHeader = "tmp/" + fname + ".preprocessed"
+        if not os.path.exists(outHeader):
+            with open(outHeader, "x"): pass
+        with open(outHeader, "w") as r:
+            r.write(output)
+
+
+preprocess()
